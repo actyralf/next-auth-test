0e4961420b83567b649fb1f8753561c61e871155
Ralf Siewert <ralf.siewert@actyvyst.com>
2023-07-04 23:56:20 +0200

    add preview workaround

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,39 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions = {
-  providers: [
+const providers = [];
+
+if (process.env.VERCEL_ENV === "preview") {
+  providers.push(
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {label: "Username", type: "text", placeholder: "fish"},
+        password: {label: "Password", type: "password"},
+      },
+      async authorize(credentials) {
+        if (
+          credentials.username === "fish" &&
+          credentials.password === "password"
+        ) {
+          return {id: "1", name: "Ralf", email: "YOUR_EMAIL@test.com"};
+        } else {
+          return null;
+        }
+      },
+    })
+  );
+} else {
+  providers.push(
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-    }),
-  ],
+    })
+  );
+}
+
+export const authOptions = {
+  providers,
 };
 export default NextAuth(authOptions);
